f7491edbea96a1fc97e3c5c736fcf2f466c49da6
MajoRC221051 <110446328+MajoRC221051@users.noreply.github.com>
2026-06-06 20:54:09 -0600

    Update app.py

--- app.py
+++ app.py
@@ -497,7 +497,7 @@ tab1, tab2, tab3, tab4, tab5, tab6 = st.tabs([
     "📈 Clima",
     "💧 Suelo",
     "🛰️ NDVI",
-    "🗺️ Mapa"
+    "🗺️ Mapa",
     "🚨 Riesgos"
 ])
 # =====================================================
